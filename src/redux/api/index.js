182df0e3764746b723063fcc98f29735eaf3ad84
Ioannis Dev <ioannis.gkigkelos@gmail.com>
2023-05-16 11:17:57 +0100

    new backend communication

--- src/redux/api/index.js
+++ src/redux/api/index.js
@@ -1,28 +1,28 @@
 import axios from "axios";
 const API = axios.create({
-    baseURL: "https://cinephillia-server-production.up.railway.app/",
+  baseURL: "https://cinephillia-server-production-ddb3.up.railway.app/",
 });
 
 API.interceptors.request.use((req) => {
-    if (localStorage.getItem("profile")) {
-        const token = JSON.parse(localStorage.getItem("profile"))?.token;
-        if (token) {
-            req.headers.authorization = `Bearer ${token}`;
-        } else {
-            req.headers.authorization = JSON.parse(
-                localStorage.getItem("profile")
-            ).userObject?.sub;
-        }
+  if (localStorage.getItem("profile")) {
+    const token = JSON.parse(localStorage.getItem("profile"))?.token;
+    if (token) {
+      req.headers.authorization = `Bearer ${token}`;
+    } else {
+      req.headers.authorization = JSON.parse(
+        localStorage.getItem("profile")
+      ).userObject?.sub;
     }
-    return req;
+  }
+  return req;
 });
 
 export const fetchFilmLists = () => API.get("/posts");
 export const fetchFilmListsByCreatorID = (id) =>
-    API.get(`/posts/creator?id=${id}`);
+  API.get(`/posts/creator?id=${id}`);
 export const createFilmList = (newFilmList) => API.post("/posts", newFilmList);
 export const updateFilmList = (id, updatedFilmList) =>
-    API.patch(`/posts/${id}`, updatedFilmList);
+  API.patch(`/posts/${id}`, updatedFilmList);
 export const deleteFilmList = (id) => API.delete(`/posts/${id}`);
 export const signIn = (formData) => API.post("/users/signin", formData);
 export const signUp = (formData) => API.post("/users/signup", formData);
